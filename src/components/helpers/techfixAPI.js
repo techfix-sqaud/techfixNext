da3db6a75838a3be350c07eb874aa997166fd414
techfix-squad <techfix-raleigh@outlook.com>
2024-06-28 17:54:30 -0400

    temp fix for baseURL

--- src/components/helpers/techfixAPI.js
+++ src/components/helpers/techfixAPI.js
@@ -1,7 +1,7 @@
-"use client";
 import axios from "axios";
-const baseURL = process.env.REACT_APP_TECHFIX_API;
-//const baseURL = "https://localhost:7032/techfix";
+//const baseURL = process.env.REACT_APP_TECHFIX_API;
+const baseURL =
+  "https://lionfish-app-rfix7.ondigitalocean.app/techfixsquad-techfix/techfix/";
 const TechFixAPI = axios.create({
   baseURL: baseURL,
   withCredentials: true,
