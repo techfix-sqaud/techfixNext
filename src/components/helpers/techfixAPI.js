d0d86d6d0240458f365362d972c7be285f3c9af6
techfix-Squad <61512454+techfix-sqaud@users.noreply.github.com>
2024-07-05 20:45:36 -0400

    Update techfixAPI.js

--- src/components/helpers/techfixAPI.js
+++ src/components/helpers/techfixAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 //const baseURL = process.env.REACT_APP_TECHFIX_API;
 const baseURL =
-  "https://lionfish-app-rfix7.ondigitalocean.app/techfixsquad-techfix/techfix/";
+  "https://www.techfix-raleigh.com/techfixsquad-techfix/techfix/";
 //const baseURL = "https://localhost:7032/techfix";
 const TechFixAPI = axios.create({
   baseURL: baseURL,
