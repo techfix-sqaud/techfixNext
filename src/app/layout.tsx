952e5867fa7a9fe48ae30c33bb21d21d921a7499
techfix-squad <techfix-raleigh@outlook.com>
2024-07-06 13:58:46 -0400

    add google anayltics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 import React, { ReactNode } from "react";
 import App from "./App";
 import { AuthProvider } from "@/components/contexts/AuthContext";
+import Script from "next/script";
 
 export const metadata = {
   title: "techfix",
@@ -15,6 +16,21 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <html lang="en">
+      <head>
+        <Script
+          strategy="lazyOnload"
+          src={`async src="https://www.googletagmanager.com/gtag/js?id=G-7CX0HWB9JS"`}
+        />
+
+        <Script id="" strategy="lazyOnload">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-7CX0HWB9JS');
+          `}
+        </Script>
+      </head>
       <body>
         <AuthProvider>
           <App>{children}</App>
