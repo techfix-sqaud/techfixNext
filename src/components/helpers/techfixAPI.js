dd85e9e40b448085babc84b1d219ba13069c565f
techfix-squad <techfix-raleigh@outlook.com>
2024-08-12 16:37:58 -0400

    fix base url

--- src/components/helpers/techfixAPI.js
+++ src/components/helpers/techfixAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 //const baseURL = process.env.REACT_APP_TECHFIX_API;
-//const baseURL = "https://www.techfix-raleigh.com/techfixsquad-techfix/techfix/";
-const baseURL = "https://localhost:7032/techfix";
+const baseURL = "https://www.techfix-raleigh.com/techfixsquad-techfix/techfix/";
+//const baseURL = "https://localhost:7032/techfix";
 const TechFixAPI = axios.create({
   baseURL: baseURL,
   withCredentials: true,
