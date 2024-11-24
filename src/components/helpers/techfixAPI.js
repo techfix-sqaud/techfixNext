7ada45856a666761556259cd9a0b28acd5b946d0
techfix-Squad <61512454+techfix-sqaud@users.noreply.github.com>
2024-11-24 13:13:12 -0500

    Update techfixAPI.js

--- src/components/helpers/techfixAPI.js
+++ src/components/helpers/techfixAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 //const baseURL = process.env.REACT_APP_TECHFIX_API;
-const baseURL = "https://www.techfixultra.com/techfix/";
+const baseURL = "https://www.techfixultra.com/techfixsquad-techfix/techfix/";
 //const baseURL = "https://localhost:7032/techfix";
 //const baseURL = "http://localhost:5127/techfix";
 const TechFixAPI = axios.create({
